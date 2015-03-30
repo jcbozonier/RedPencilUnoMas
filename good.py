dbcb13a60a6a5cc8ffda46ee7948bd2412ff3676
Justin Bozonier <darkxanthos@gmail.com>
2015-03-30 03:44:56 -0400

    Basic percent range established.

--- good.py
+++ good.py
@@ -2,7 +2,7 @@ class Good():
     def __init__(self):
         self.red_pencil_promotion_active = False
     def reduce_price(self, by):
-        if by == 0.05:
+        if by >= 0.05 and by <= 0.3:
             self.red_pencil_promotion_active = True
     def is_red_pencil_promotion_active(self):
         return self.red_pencil_promotion_active
--- tests/red_pencil_tests.py
+++ tests/red_pencil_tests.py
@@ -11,3 +11,11 @@ class given_a_fresh_good_with_no_history(unittest.TestCase):
     def test_when_price_reduced_by_5_percent(self):
         self.good.reduce_price(by=.05)
         self.assertTrue(self.good.is_red_pencil_promotion_active(), "It should activate the red pencil promotion")
+        self.good = Good()
+    def test_when_price_reduced_by_30_percent(self):
+        self.good.reduce_price(by=.30)
+        self.assertTrue(self.good.is_red_pencil_promotion_active(), "It should NOT activate the red pencil promotion")
+        self.good = Good()
+    def test_when_price_reduced_by_31_percent(self):
+        self.good.reduce_price(by=.31)
+        self.assertFalse(self.good.is_red_pencil_promotion_active(), "It should NOT activate the red pencil promotion")
